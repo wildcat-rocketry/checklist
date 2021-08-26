bfdbcdc13b20a142aec0db5bcde88ae1fc42bcf3
Zach Deibert <zachdeibert@gmail.com>
2021-08-26 16:21:02 -0500

    Added more documentation

--- index.js
+++ index.js
@@ -5,28 +5,44 @@
         "name": "Introduction",
         "courses": [
             {
-                "name": "Say \"Hello, World!\" with C++"
+                "name": "Exercise: Say \"Hello, World!\" with C++",
+                "href": "https://www.hackerrank.com/challenges/cpp-hello-world/problem",
+                "target": "exercise"
             },
             {
-                "name": "Input and Output"
+                "name": "Exercise: Input and Output",
+                "href": "https://www.hackerrank.com/challenges/cpp-input-and-output/problem",
+                "target": "exercise"
             },
             {
-                "name": "Basic Data Types"
+                "name": "Exercise: Basic Data Types",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem",
+                "target": "exercise"
             },
             {
-                "name": "Conditional Statements"
+                "name": "Exercise: Conditional Statements",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem",
+                "target": "exercise"
             },
             {
-                "name": "For Loop"
+                "name": "Exercise: For Loop",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem",
+                "target": "exercise"
             },
             {
-                "name": "Functions"
+                "name": "Exercise: Functions",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-functions/problem",
+                "target": "exercise"
             },
             {
-                "name": "Pointer"
+                "name": "Exercise: Pointer",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-pointer/problem",
+                "target": "exercise"
             },
             {
-                "name": "Arrays Introduction"
+                "name": "Exercise: Arrays Introduction",
+                "href": "https://www.hackerrank.com/challenges/arrays-introduction/problem",
+                "target": "exercise"
             }
         ]
     },
@@ -34,19 +50,44 @@
         "name": "Classes",
         "courses": [
             {
-                "name": "StringStream"
+                "name": "Exercise: StringStream",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem",
+                "target": "exercise"
             },
             {
-                "name": "Class"
+                "name": "Exercise: Class",
+                "href": "https://www.hackerrank.com/challenges/c-tutorial-class/problem",
+                "target": "exercise"
             },
             {
-                "name": "Classes and Objects"
+                "name": "Exercise: Classes and Objects",
+                "href": "https://www.hackerrank.com/challenges/classes-objects/problem",
+                "target": "exercise"
             },
             {
-                "name": "Box It!"
+                "name": "Microsoft Docs: Member Access Control",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/member-access-control-cpp",
+                "target": "docs"
             },
             {
-                "name": "Virtual Functions"
+                "name": "Microsoft Docs: Constructors",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/constructors-cpp",
+                "target": "docs"
+            },
+            {
+                "name": "Exercise: Box It!",
+                "href": "https://www.hackerrank.com/challenges/box-it/problem",
+                "target": "exercise"
+            },
+            {
+                "name": "Microsoft Docs: Virtual Functions",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/virtual-functions",
+                "target": "docs"
+            },
+            {
+                "name": "Exercise: Virtual Functions",
+                "href": "https://www.hackerrank.com/challenges/virtual-functions/problem",
+                "target": "exercise"
             }
         ]
     },
@@ -54,24 +95,24 @@
         "name": "Inheritance",
         "courses": [
             {
-                "name": "Inheritance Introduction"
+                "name": "Exercise: Inheritance Introduction",
+                "href": "https://www.hackerrank.com/challenges/inheritance-introduction/problem",
+                "target": "exercise"
             },
             {
-                "name": "Rectangle Area"
+                "name": "Microsoft Docs: Single Inheritance",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/single-inheritance",
+                "target": "docs"
             },
             {
-                "name": "Multi-Level Inheritance"
+                "name": "Exercise: Rectangle Area",
+                "href": "https://www.hackerrank.com/challenges/rectangle-area/problem",
+                "target": "exercise"
             },
             {
-                "name": "Accessing Inherited Functions"
-            }
-        ]
-    },
-    {
-        "name": "Debugging",
-        "courses": [
-            {
-                "name": "Cpp exception handling"
+                "name": "Exercise: Multi-Level Inheritance",
+                "href": "https://www.hackerrank.com/challenges/multi-level-inheritance-cpp/problem",
+                "target": "exercise"
             }
         ]
     },
@@ -79,13 +120,39 @@
         "name": "Other Concepts",
         "courses": [
             {
-                "name": "C++ Class Templates"
+                "name": "Microsoft Docs: try, throw, and catch Statements",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/try-throw-and-catch-statements-cpp",
+                "target": "docs"
             },
             {
-                "name": "Overload Operators"
+                "name": "Exercise: Cpp exception handling",
+                "href": "https://www.hackerrank.com/challenges/cpp-exception-handling/problem",
+                "target": "exercise"
             },
             {
-                "name": "C++ Class Template Specialization"
+                "name": "Microsoft Docs: References",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/references-cpp",
+                "target": "docs"
+            },
+            {
+                "name": "Microsoft Docs: Templates",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/templates-cpp",
+                "target": "docs"
+            },
+            {
+                "name": "Exercise: C++ Class Templates",
+                "href": "https://www.hackerrank.com/challenges/c-class-templates/problem",
+                "target": "exercise"
+            },
+            {
+                "name": "Microsoft Docs: Arrays",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/arrays-cpp",
+                "target": "docs"
+            },
+            {
+                "name": "Microsoft Docs: Lambda Expressions",
+                "href": "https://docs.microsoft.com/en-us/cpp/cpp/lambda-expressions-in-cpp",
+                "target": "docs"
             }
         ]
     }
@@ -100,7 +167,9 @@
     part.courses.forEach(course => {
         document.write("    <tr>");
         document.write("        <td class=\"mdl-data-table__cell--non-numeric\">");
+        document.write(`    <a href=\"${course.href}\" target="${course.target}">`);
         document.write(`            ${course.name}`);
+        document.write("    </a>");
         document.write("        </td>");
         document.write("    </tr>");
     });
